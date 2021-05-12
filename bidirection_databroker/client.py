c88e98c4943a595933d07c034554a4c90994cae3
ipa-rar <ragesh.ramachandran.ipa.fraunhofer.de>
2021-05-12 15:50:37 +0200

    bidirectional streaming server issue fixed

--- bidirection_databroker/client.py
+++ bidirection_databroker/client.py
@@ -1,10 +1,11 @@
-import grpc
 import time
 import csv
+import grpc
 
 from generated import broker_pb2 as msg
 from generated import broker_pb2_grpc as Client
-port = 'localhost:8061'
+PORT = 'localhost:8061'
+
 
 def bidirectional_streaming(stub):
 
@@ -14,29 +15,29 @@ def bidirectional_streaming(stub):
             row = csv.reader(dataset, delimiter=",")
             for i, data in enumerate(row):
                 request = msg.BrokerRequest(id=int(data[0]),
-                sensor1=float(data[1]),
-                sensor2=float(data[2]),
-                sensor3=float(data[3]),
-                sensor4=float(data[4]))
+                                            sensor1=float(data[1]),
+                                            sensor2=float(data[2]),
+                                            sensor3=float(data[3]),
+                                            sensor4=float(data[4]))
                 yield request
-                time.sleep(.0001) 
+                time.sleep(.001)
 
     response_iterator = stub.BidirectionalStreaming(stream_messages())
 
     for response in response_iterator:
         print(response.id,
-        response.prediction)
+              response.prediction)
+
 
 def main():
-    with grpc.insecure_channel(port) as channel:
+    with grpc.insecure_channel(PORT) as channel:
         stub = Client.BrokerServiceStub(channel)
         bidirectional_streaming(stub)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
         print("Interrupted!")
         exit(0)
-
-        
--- bidirection_databroker/server.py
+++ bidirection_databroker/server.py
@@ -1,39 +1,46 @@
-import grpc
 from concurrent import futures
 from threading import Thread
+import logging
+import grpc
 
 from generated import broker_pb2 as msg
 from generated import broker_pb2_grpc as Server
-port = 8061
+PORT = 8061
+
 
 class BrokerServiceServicer(Server.BrokerServiceServicer):
 
-    def ProcessRequest(self, request_iterator):
+    def __call__(self, request_iterator):
+        """Receives the client request stream and process it"""
         for request in request_iterator:
-            print(request.id, 
-                request.sensor1,
-                request.sensor2,
-                request.sensor3,
-                request.sensor4)
-                
-        
+            logging.info("request")
+            print(request.id,
+                  request.sensor1,
+                  request.sensor2,
+                  request.sensor3,
+                  request.sensor4)
+            
+
     def BidirectionalStreaming(self, request_iterator, context):
-        p1 = Thread(target=self.ProcessRequest(request_iterator))
-        p1.start()
-        ## apply some algorithm here on the received data
-        self.ProcessRequest(request_iterator)
-        yield msg.BrokerResponse(id = 15, prediction = True)
-        p1.join()
+        """Server request callback function"""
+        process = Thread(target=self, args=(request_iterator,)) 
+        process.start()
+        yield msg.BrokerResponse(id=15, prediction=True)
+        # apply some algorithm here on the received data
+        logging.warning("foobar")
+        process.join()
+
 
 def main():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     Server.add_BrokerServiceServicer_to_server(BrokerServiceServicer(), server)
-    print("Starting server. Listening on port : " + str(port))
-    server.add_insecure_port("[::]:{}".format(port))
+    print("Starting server. Listening on port : " + str(PORT))
+    server.add_insecure_port("[::]:{}".format(PORT))
     server.start()
     server.wait_for_termination()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
